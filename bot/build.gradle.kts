4586d47782d4ae2c1ae58f9a47dd782ea32dce29
Alexander Bulatov <aleks40k@yandex.ru>
2025-08-06 23:15:31 +0300

    feat: add compile and test deps

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -14,15 +14,48 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("io.quarkus.platform:quarkus-bom:" + properties["quarkusPlatformVersion"]))
-    implementation("io.quarkus:quarkus-grpc")
-    implementation("org.ta4j:ta4j-core:" + properties["ta4jVersion"])
+    implementation(
+        platform("io.quarkus.platform:quarkus-bom:" + properties["quarkusPlatformVersion"]),
+        "io.quarkus:quarkus-grpc",
+        "org.ta4j:ta4j-core:" + properties["ta4jVersion"]
+    )
+    compileOnly(
+        "ru.tinkoff.piapi:java-sdk-grpc-contract:" + properties["tinkoffApiVersion"],
+        "org.mapstruct:mapstruct:" + properties["mapstructVersion"],
+        "org.projectlombok:lombok:" + properties["lombokVersion"]
+    )
+    annotationProcessor(
+        "org.projectlombok:lombok:" + properties["lombokVersion"],
+        "org.mapstruct:mapstruct-processor:" + properties["mapstructVersion"],
+        "org.projectlombok:lombok-mapstruct-binding:" + properties["lombokMapstructBindingVersion"]
+    )
 
-    compileOnly("ru.tinkoff.piapi:java-sdk-grpc-contract:" + properties["tinkoffApiVersion"])
-
+    testImplementation(
+        "io.quarkus:quarkus-junit5",
+        "io.quarkus:quarkus-junit5-mockito"
+        )
+    testCompileOnly("org.projectlombok:lombok:" + properties["lombokVersion"])
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+fun DependencyHandler.implementation(vararg deps: Any) {
+    for (dep in deps) {
+        add("implementation", dep)
+    }
+}
+
+fun DependencyHandler.annotationProcessor(vararg deps: Any) {
+    for (dep in deps) {
+        add("annotationProcessor", dep)
+    }
+}
+
+fun DependencyHandler.testImplementation(vararg deps: Any) {
+    for (dep in deps) {
+        add("testImplementation", dep)
+    }
 }
