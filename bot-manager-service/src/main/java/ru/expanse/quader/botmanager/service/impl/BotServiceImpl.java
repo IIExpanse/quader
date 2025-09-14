f943bddc8405d2bcfb4aef3a750160159f19c2f3
Alexander Bulatov <aleks40k@yandex.ru>
2025-09-14 15:16:07 +0300

    fix: add bean declaration

--- bot-manager-service/src/main/java/ru/expanse/quader/botmanager/service/impl/BotServiceImpl.java
+++ bot-manager-service/src/main/java/ru/expanse/quader/botmanager/service/impl/BotServiceImpl.java
@@ -1,11 +1,13 @@
 package ru.expanse.quader.botmanager.service.impl;
 
 import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
 import ru.expanse.quader.botmanager.client.BotClientAdapter;
 import ru.expanse.quader.botmanager.resolver.BotDataResolver;
 import ru.expanse.quader.botmanager.service.BotService;
 
+@ApplicationScoped
 @RequiredArgsConstructor
 public class BotServiceImpl implements BotService {
     private final BotClientAdapter botClientAdapter;
